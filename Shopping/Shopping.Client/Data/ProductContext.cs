a6e913d0f9b1bd65078083a62a2eb93efa5064b3
Sachidanand Suraj <ssuraj@intelliflo.com>
2022-01-14 22:28:30 +0530

    initial push

---
+++ Shopping/Shopping.Client/Data/ProductContext.cs
@@ -0,0 +1,55 @@
+﻿using Shopping.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Client.Data
+{
+    public class ProductContext
+    {
+        public static readonly List<Product> Products = new List<Product>
+        {
+            new Product()
+            {
+                Name = "Iphone X",
+                Description = "kjsadfklsdjf klsdjflksjlfksjdlkfjsdlkfj",
+                ImageFile = "product1.png",
+                Price = 950.00M,
+                Category = "Smart-Phone"
+            },
+            new Product()
+            {
+                Name = "Iphone X",
+                Description = "kjsadfklsdjf klsdjflksjlfksjdlkfjsdlkfj",
+                ImageFile = "product1.png",
+                Price = 950.00M,
+                Category = "Smart-Phone"
+            },
+            new Product()
+            {
+                Name = "Iphone X",
+                Description = "kjsadfklsdjf klsdjflksjlfksjdlkfjsdlkfj",
+                ImageFile = "product1.png",
+                Price = 950.00M,
+                Category = "Smart-Phone"
+            },
+            new Product()
+            {
+                Name = "Iphone X",
+                Description = "kjsadfklsdjf klsdjflksjlfksjdlkfjsdlkfj",
+                ImageFile = "product1.png",
+                Price = 950.00M,
+                Category = "Smart-Phone"
+            },
+            new Product()
+            {
+                Name = "Iphone X",
+                Description = "kjsadfklsdjf klsdjflksjlfksjdlkfjsdlkfj",
+                ImageFile = "product1.png",
+                Price = 950.00M,
+                Category = "Smart-Phone"
+            }
+        };
+    }
+}
